d4b3386d6ba1a4a4159135efca64c2b8f10906ac
Dylan Mathiesen <dylanmathiesen@gmail.com>
2013-08-20 14:38:49 +1000

    s

--- cgi/params.py
+++ cgi/params.py
@@ -7,7 +7,7 @@ from settings import Settings
 print "Content-type: text/html"
 print
 
-def cgiFieldStorageToDict( fieldStorage ):
+def cgiFieldStorageToDict(fieldStorage):
    """Get a plain dictionary, rather than the '.value' system used by the cgi module."""
    params = {}
    for key in fieldStorage.keys():
@@ -15,33 +15,33 @@ def cgiFieldStorageToDict( fieldStorage ):
    return params
 
 def test():
-	print 'Running'
+    print 'Running'
 
 def listSchedules():
-	print 'sched'
-	form = cgi.FieldStorage()
-	formDict = cgiFieldStorageToDict(form)	
+    print 'sched'
+    form = cgi.FieldStorage()
+    formDict = cgiFieldStorageToDict(form)    
 
-	print formDict
-	print "<br/>"
+    print formDict
+    print "<br/>"
 
-	if ('type' not in formDict):
-		print 'Invalid form'
-		return 0
-	
-	type = formDict['type']
-	print formDict['type'] + "<br/>"
-	
-	if (type == 'notification'):
-		setEmail(formDict['inputEmail'],True);
-	elif (type == 'turningp'):
-		pass
-	elif (type == 'schedule'):
-		addSchedule(formDict['name'],formDict['schedule'])
+    if ('type' not in formDict):
+        print 'Invalid form'
+        return 0
+    
+    type = formDict['type']
+    print formDict['type'] + "<br/>"
+    
+    if (type == 'notification'):
+        setEmail(formDict['inputEmail'], True);
+    elif (type == 'turningp'):
+        pass
+    elif (type == 'schedule'):
+        addSchedule(formDict['name'], formDict['schedule'])
     elif (type == 'settings'):
         settingsFile()
-	else:
-		print 'unknown type'
+    else:
+        print 'unknown type'
 
 def settingsFile():
     emails = ",".join(cfg.getEmails())
@@ -52,21 +52,21 @@ def settingsFile():
     print 'Schedule:' + schedule[0] + ',' + schedule[1]
 
 def addSchedule(name, schedule):
-	if (schedule == ''):
-		#delete it
-		pass
-	else:
-		schedule = open('schedules/'+name+'.csv', 'w')
-		schedule.write(schedule)
-        cfg.addSchedule(name,schedule)
-		schedule.close()
+    if (schedule == ''):
+        #delete it
+        pass
+    else:
+        schedule = open('schedules/' + name + '.csv', 'w')
+        schedule.write(schedule)
+        cfg.addSchedule(name, schedule)
+        schedule.close()
 
 
 def setEmail(email, add=True):
-	if add:
-		cfg.addEmail(email)
-	else:
-		cfg.deleteEmail(email)
+    if add:
+        cfg.addEmail(email)
+    else:
+        cfg.deleteEmail(email)
 
 
 cfg = Settings()
